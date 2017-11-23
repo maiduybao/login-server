1d85d646b4301d5e4d1e7a8d52754419579d7547
Bao Mai <maiduybao@gmail.com>
2017-11-23 12:00:25 -0800

    move the all secret keys to environment setting

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,9 @@
 import config from "./config.json";
 
 config.mailgun.apiKey = process.env.MAILGUN_API_KEY;
+if (process.env.JWT_SECRET_KEY) {
+    config.jwt.secretKey = process.env.JWT_SECRET_KEY;
+}
 // export default config;
 export const {database: dbConfig, log: logConfig, jwt: jwtConfig, mailgun: mailConfig} = config;
 
