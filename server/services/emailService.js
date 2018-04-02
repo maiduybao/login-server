4217b897d29b9ee9c87cebab80ee1edde7598a92
Bao Mai <maiduybao@gmail.com>
2018-04-01 20:19:08 -0700

    npm update

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,6 +1,8 @@
 import log4js from "log4js";
 import MailGun from "mailgun-js";
 
+import EmailTemplate from "email-templates";
+
 import {mailConfig} from "../config";
 
 const logger = log4js.getLogger("EmailService");
@@ -18,6 +20,16 @@ class EmailService {
             subject: "Welcome to login demo",
             text: `Thank you for registering. Please use the token below to activate your profile.\nToken:${newUser.confirmToken}`
         };
+
+
+        const email = new EmailTemplate();
+
+        email.renderAll("loginactivate", {token: newUser.confirmToken})
+            .then((template) => {
+                logger.debug("loginactivate", template);
+            })
+            .catch(logger.error);
+
 
         return new Promise((resolve, reject) => {
             mailgun.messages().send(message, (error, body) => {
